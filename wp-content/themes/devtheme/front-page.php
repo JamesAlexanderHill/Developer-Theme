0637d5c06178ff75c156ff80ee9da63a758c1a63
James Hill <jhill7177@gmail.com>
2020-04-09 01:09:48 +1000

    init
    - set current homepage to front-page.php
    - finalised gitignore
    - set up basic files

---
+++ wp-content/themes/devtheme/front-page.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>James Hill</title>
+<style>
+  *{
+    padding: 0px;
+    margin: 0px;
+    font-family: sans-serif;
+    font-weight: lighter;
+  }
+  body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	min-height: 100vh;
+  }
+  h1, h2, p{
+    text-align: center;
+    color: #727272;
+  }
+  a{
+    color: darkgrey;
+  }
+  a:hover{
+    color: darkblue;
+  }
+
+</style>
+</head>
+<body>
+  <header></header>
+  <div id="content">
+    <h1>This domain is owned by:</h1>
+    <h2>James Hill</h2>
+    <br>
+    <p><a href="mailto:jhill7177@gmail.com">Email Me</a></p>
+  </div>
+  <footer></footer>
+</body>
+</html>
