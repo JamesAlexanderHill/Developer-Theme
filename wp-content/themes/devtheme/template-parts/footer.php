195031b4ddf2e3ef554f2f21e8a16bbe9a265031
James Hill <jhill7177@gmail.com>
2020-05-07 01:47:36 +1000

    changed menus to have container id + container elements

--- wp-content/themes/devtheme/template-parts/footer.php
+++ wp-content/themes/devtheme/template-parts/footer.php
@@ -1,39 +1,41 @@
       <!-- start footer.php -->
       <footer class="page-width">
-        <nav id="footer-nav">
-          <!-- <ul>
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location' => 'main-menu',
+            'container' => 'nav',
+            'container_id' => 'footer-nav',
+            'fallback_cb' => false
+          )
+        );
+        ?>
+        <!-- <nav id="footer-nav">
+          <ul>
             <li><a href="#">About Me</a></li>
             <li><a href="#">Resume</a></li>
             <li><a href="#">Projects</a></li>
             <li><a href="#">Dev-Notes</a></li>
             <li><a href="#">Contact</a></li>
-          </ul> -->
-          <?php
-          wp_nav_menu(
-            array(
-              'theme_location' => 'main-menu',
-              'theme_location' => '__no_such_location',
-              'fallback_cb' => false
-            )
-          );
-          ?>
-        </nav>
-        <nav id="social-nav">
-          <!-- <ul>
+          </ul>
+        </nav> -->
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location' => 'main-menu',
+            'container' => 'nav',
+            'container_id' => 'main-nav',
+            'fallback_cb' => false
+          )
+        );
+        ?>
+        <!-- <nav id="social-nav">
+          <ul>
             <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="#"><i class="fab fa-github-square"></i></a></li>
             <li><a href="#"><i class="fas fa-envelope-square"></i></a></li>
-          </ul> -->
-          <?php
-          wp_nav_menu(
-            array(
-              'theme_location' => 'social-menu',
-              'theme_location' => '__no_such_location',
-              'fallback_cb' => false
-            )
-          );
-          ?>
-        </nav>
+          </ul>
+        </nav> -->
       </footer>
     </div>
     <?php wp_footer(); ?>
--- wp-content/themes/devtheme/template-parts/header.php
+++ wp-content/themes/devtheme/template-parts/header.php
@@ -8,44 +8,48 @@
     <!-- <script src="https://kit.fontawesome.com/037b8e2be6.js" crossorigin="anonymous"></script> -->
   </head>
   <body <?php body_class(); ?>>
-    <nav id="side-nav">
-      <!-- <ul>
+    <?php
+    wp_nav_menu(
+      array(
+        'theme_location' => 'main-menu',
+        'container' => 'nav',
+        'container_id' => 'side-nav',
+        'fallback_cb' => false
+      )
+    );
+    ?>
+    <!-- <nav id="side-nav">
+      <ul>
         <li><a href="#">About Me</a></li>
         <li><a href="#">Resume</a></li>
         <li><a href="#">Projects</a></li>
         <li><a href="#">Dev-Notes</a></li>
         <li><a href="#">Contact</a></li>
-      </ul> -->
-      <?php
-      wp_nav_menu(
-        array(
-          'theme_location' => 'main-menu',
-          'theme_location' => '__no_such_location',
-          'fallback_cb' => false
-        )
-      );
-      ?>
-    </nav>
+      </ul>
+    </nav> -->
     <div id="container">
       <header class="page-width">
         <div id="navToggleBtnContainer"><button onclick="toggleNav()"><i class="fas fa-bars"></i><i style="display: none;" class="fas fa-times"></i></button></div>
         <div>James Hill</div>
-        <nav id="main-nav">
-          <!-- <ul>
+        <?php
+        wp_nav_menu(
+          array(
+            'theme_location' => 'main-menu',
+            'container' => 'nav',
+            'container_id' => 'main-nav',
+            'fallback_cb' => false
+          )
+        );
+        ?>
+        <!-- <nav id="main-nav">
+          <ul>
             <li><a href="#">About Me</a></li>
             <li><a href="#">Resume</a></li>
             <li><a href="#">Projects</a></li>
             <li><a href="#">Dev-Notes</a></li>
             <li><a href="#">Contact</a></li>
-          </ul> -->
-          <?php
-          wp_nav_menu(
-            array(
-              'theme_location' => 'main-menu'
-            )
-          );
-          ?>
-        </nav>
+          </ul>
+        </nav> -->
         <div id="downloadBtnContainer"><i class="fas fa-file-download"></i></div>
       </header>
       <!-- end header.php -->
